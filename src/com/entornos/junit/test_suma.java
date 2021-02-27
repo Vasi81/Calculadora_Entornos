785a21bef4210ca4a014bda64ff49bf11f4758e8
pepmarq22 <79054679+pepmarq22@users.noreply.github.com>
2021-02-27 10:43:35 +0100

    Add files via upload

---
+++ src/com/entornos/junit/test_suma.java
@@ -0,0 +1,98 @@
+package com.entornos.junit;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+import com.entornos.calculadora.Suma;
+class test_suma {
+	
+
+
+	@Test
+	 void testSumaEnteros()throws Exception {		
+		int operador1= 5;
+		int operador2= 6;
+		
+		Suma s1 = new Suma();
+		
+		assertEquals(11,s1.sumar(operador1, operador2));
+		System.out.println("Test cálculo suma con números enteros fue correcto");
+		
+	}
+	
+	
+	
+	
+	@Test
+	void testSumaReal() throws Exception {
+		double operador3 = 3.0;
+		double operador4 = 6.0;
+		double operador5 = 1.0;
+
+		
+		Suma s1 = new Suma();
+		
+
+		assertEquals(10,s1.sumar(operador3, operador4,operador5),0);
+		System.out.println("Test cálculo suma con números reales fue correcto");
+
+	}
+	
+
+	
+	@Test
+	void testSumaRea() throws Exception {
+		double operador6 = 6.5;
+		double operador7 = 1.5;
+		
+		Suma s1 = new Suma();
+		
+		assertEquals(8,s1.sumar(operador6,operador7),0);
+		System.out.println("Test cálculo suma con números reales fue correcto");
+	}
+	
+	@Test
+	void testUnicoEntero() throws Exception {
+	
+		int num=5;
+		
+		Suma s1 = new Suma();
+		
+		assertEquals(16,s1.sumar(num),0);
+		System.out.println("Test cálculo suma con números reales fue correcto");
+	}
+	
+	@Test
+	void testUnicoReal() throws Exception {
+	
+		double num=1.0;
+		
+		Suma s1 = new Suma();
+		
+		System.out.println(s1.acumuladorsumareal);
+		
+		assertEquals(11,s1.sumar(num),0);
+		System.out.println("Test cálculo suma con números reales fue correcto");
+}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
